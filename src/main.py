40aa7b4847a81a37eb095783c5a55586b9720d75
Jonathan <jonathan.jnthnd@gmail.com>
2023-05-02 17:39:02 -0500

    Arreglo al importar "data"

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import mainDeck
-import datos
-test = datos.DatosDePrueba.data_array
-deckTest = mainDeck.MainDeck(len(test))
-for i in test:
+import sys
+import os
+ruta_carpeta1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+sys.path.append(ruta_carpeta1)
+print(sys.path)
+import DatosDePrueba
+deckTest = mainDeck.MainDeck(len(DatosDePrueba.data_array))
+for i in DatosDePrueba.data_array:
     deckTest.PushBack(i)
 deckTest.DeckPrint()
 print()
-print("Ahora vuelto mierda")
+print("<----  Shuffle  --->")
 deckTest.DeckShuffle()
 print()
 deckTest.DeckPrint()
