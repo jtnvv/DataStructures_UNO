b4f24f01a669a8eb7b024c7d0a240ecaadfded5e
Juan Ovalle <130797033+JOvalleG@users.noreply.github.com>
2023-05-02 20:36:36 -0500

    Se añade la funcion de vaciar DiscardDeck
    Se vacia DiscardDeck cuando esta tenga 50 elementos y conservar la ultima carta jugada

--- src/discardDeck.py
+++ src/discardDeck.py
@@ -9,6 +9,10 @@ class DiscardDeck:
   def __init__(self):
     self.head = None
     self.tail = None
+    self.num_cards = 0
+
+  def is_empty(self):
+    return self.head == None
 
   def Enqueue(self,key):
     node = Node(key)
@@ -18,19 +22,41 @@ class DiscardDeck:
     else:
       self.tail.next = node
       self.tail = node
+    self.num_cards += 1
+    return
 
   def Dequeue(self):
+    if self.is_empty():
+      return None
+    if self.head == self.tail:
+      self.tail = None
     item = self.head.data
     self.head = self.head.next
+    self.num_cards -= 1
     return item
     
-  def DeckPrint(self):
-    node = self.head
-    while (node.next!=None):
-        print(node.data, end=" ")
-        node = node.next
-    print(node.data, end="")
+  def printDeck(self):
+    if self.is_empty():
+      print("Deck is empty.")
+      return
+
+    current_node = self.head
+    while current_node:
+      print(current_node.data)
+      current_node = current_node.next
   
   def LastCardPlayed(self):
     return self.head.data
+  
+  def is_full(self):
+    if self.num_cards == 10:
+      top = self.tail.data
+      self.empty()
+      self.Enqueue(top)
+    
+  def empty(self):
+    while not self.is_empty():
+        self.Dequeue()
+    
+  
   
