17799b5b9d2f62103912b291ff49e72f5302ac30
Jonathan David <90337584+jtnvv@users.noreply.github.com>
2023-08-28 21:35:35 -0500

    Update DatosDePrueba.py

--- data/DatosDePrueba.py
+++ data/DatosDePrueba.py
@@ -4,6 +4,6 @@ from pathlib import Path
 #data_file_path = Path(__file__).parent / "MOCK_DATA (10).json"
 #data_file_path = Path(__file__).parent / "MOCK_DATA (100).json"
 data_file_path = Path(__file__).parent / "MOCK_DATA_FINAL.json"
-data_array = json.load(open(data_file_path))
+data_array = json.load(open(data_file_path)
 
 
